b20c81b3f9303607b5817c5fb4aebe05907ca980
rafael-cena <rafaelcena4@gmail.com>
2024-08-23 08:55:30 -0300

    corrigindo o funcionamento do componente Data

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="App">
       <Data texto="Data e hora local: " timeZone = "-3.00" />
-      <Data texto="Data e hora em Londres: " timeZone = "0.00" />
-      <Data texto="Data e hora em Nova York: " timeZone = "-5.00" />
+      <Data texto="Data e hora em Londres: " timeZone = "1.00" />
+      <Data texto="Data e hora em Nova York: " timeZone = "-4.00" />
     </div>
   );
 }
--- src/meusComponentes/Data.js
+++ src/meusComponentes/Data.js
@@ -1,38 +1,46 @@
 import { Component } from "react";
 
 export default class Data extends Component {
-    
-    constructor (props) {
+
+    constructor(props) {
         super(); //executa o constrtor do pai
         this.props = props;
         this.state = {
-            dataAtual : new Date().toLocaleString()
+            dataAtual: new Date().toLocaleString()
         }
     }
 
-    //fase de montagem
-    componentDidMount () {
-        console.log("O componente foi montado.");
-        //nao é permitido atualizar o estado do componente diretamente
-        // this.state = 
-        this.setState({
-            dataAtual : new Date().toLocaleString()
-        })
+    pegaDataDe(timeZone) {
+        const dataAtual = new Date();
+        let timeZoneFromDB = parseInt(timeZone);
+        let diferencaTempo = timeZoneFromDB * 60 + dataAtual.getTimezoneOffset();
+        let milisecundos = parseInt(dataAtual.getTime() + diferencaTempo * 60 * 1000)
+        const data = new Date(milisecundos);
+        return data;
     }
 
-    componentDidUpdate () {
-        console.log("O componente foi atualizado.");
+    //fase de montagem
+    componentDidMount() {
+        console.log("O componente foi montado.");
+        //Não é permitido atualizar o estado do componente de forma direta
+        //this.state =  ...
+        this.setState({
+            dataAtual: new Date().toLocaleString()
+        });
+    }
+
+    componentDidUpdate() {
+        console.log("O componente foi atualizado");
         setTimeout(() => {
             this.setState({
-                dataAtual : new Date().toLocaleString()
-            })
+                dataAtual: this.pegaDataDe(this.props.timeZone).toLocaleString()
+            });
         }, 1000);
     }
-
-    // sobrescrita de método
+    //sobrescrita de método
     render() {
         return (
-            <h1>{ this.props.texto || "" } {this.state.dataAtual}</h1>
+            <h1>{this.props.texto || ""}{this.state.dataAtual}</h1>
         )
     }
 }
